b6b9cc2e142dde5dc8f4b21b82f5c8f105789d7d
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-09-06 13:45:09 -0500

    initial commit, .env file created, database added, database name changed in pool.js

--- database.sql
+++ database.sql
@@ -5,6 +5,25 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "username" VARCHAR (255) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "palette" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"palette_name" varchar(255) NOT NULL,
+	"user_id" int REFERENCES "user" NOT NULL,
+	"hex0" varchar(7) NOT NULL,
+	"hex1" varchar(7) NOT NULL,
+	"hex2" varchar(7) NOT NULL,
+	"hex3" varchar(7) NOT NULL,
+	"hex4" varchar(7) NOT NULL,
+	"hex5" varchar(7) NOT NULL,
+	"img_url" varchar(2083) NOT NULL,
+	"quantity0" int NOT NULL,
+	"quantity1" int NOT NULL,
+	"quantity2" int NOT NULL,
+	"quantity3" int NOT NULL,
+	"quantity4" int NOT NULL,
+	"quantity5" int NOT NULL
+	);
