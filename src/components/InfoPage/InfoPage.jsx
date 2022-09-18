43021d3ed4e74bfd7cb1350412e2e6f43148af9f
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-09-18 09:11:29 -0500

    added info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -8,16 +8,29 @@ import React from 'react';
 function InfoPage() {
   return (
     <>
+    
     <div className="container">
-      <h3>info page</h3>
-    </div>
-    <div>
-      <h3>Technologies Used</h3>
-      <p> React libraries--React-color-extractor&React-color-picker, React, Redux, Redux-Saga, Node, Express, SQL, Postgresql, Heroku, CSS</p>
-      <h3>Looking ahead...</h3>
-      <p>I would love to continue playing with different ways of customizing the palette</p>
-      <p>Work with Multer so a user can upload their own image</p>
-      <h3>Thank you to the Mitchison Cohort, Dane, Kris, Liz! </h3>
+      <h2>Technologies Used:</h2>
+      <ul>
+        <li>React libraries (<i>React-color-extractor</i> & <i>React-color-picker</i>)</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Redux-Saga</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>SQL</li>
+        <li>Postgresql</li>
+        <li>Heroku</li>
+        <li>CSS</li>
+      </ul>
+     
+      <h2>Looking ahead...</h2>
+      <ol>
+        <li>Further palette customization</li>
+        <li>Use Multer API to allow users to upload their personal images</li>
+      </ol>
+    
+      <h1>Thank you to the Mitchison Cohort, Dane, Kris, Liz! </h1>
     </div>
     </>
   );
